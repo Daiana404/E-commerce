42660982e935488584a0dbab135caec62a1f7389
Daiana Polo <daia_p@hotmail.com.ar>
2023-03-16 15:42:55 -0300

    Nav y footer

--- js/navbar.js
+++ js/navbar.js
@@ -1,48 +1,74 @@
-const boxBuscador = document.querySelector('.box-buscador');
+//Dom
+const cajaBuscador = document.querySelector('.caja-buscador');
 const btnBuscar = document.querySelector('.btn-buscar');
 const btnEnviar = document.querySelector('.btn-enviar');
-const input = boxBuscador.firstElementChild;
+const grupo = document.querySelector('.grupo-desplegable');
+const navegador = document.querySelector('.navegador');
 const btnMenu = document.querySelector('.btn-menu');
-const grupo = document.querySelector('.grupo');
-const navegador = document.getElementById('navegador');
-const btnCerrarNav = document.querySelector('.btn-cerrar-nav');
-
-btnMenu.addEventListener('click', () => {
-    navegador.classList.add('show');
-    grupo.classList.add('show');
-    boxBuscador.classList.add('show');
-    btnMenu.style.opacity = 0;
-})
-
-btnCerrarNav.addEventListener('click', () => {
-    
-})
+const nombre = document.querySelector('.nombre');
+const btnCerrar = document.querySelector('.btn-cerrar');
+const inputBuscar = document.querySelector('#input-buscar');
+const main = document.querySelector('.principal');
+const encabezado = document.getElementById('encabezado');
+//Buscador
+let esconderBuscador = () => {
+    cajaBuscador.style.visibility = 'hidden';
+    cajaBuscador.style.opacity = 0;
+    cajaBuscador.style.transform = 'translateY(0%)';
+}
+let mostrarBuscador = () => {
+    cajaBuscador.style.visibility = 'visible';
+    cajaBuscador.style.opacity = 1;
+    cajaBuscador.style.transform = 'translateY(100%)';
+}
 
 btnBuscar.addEventListener('click', () => {
-    boxBuscador.classList.add('active');
-    btnBuscar.classList.add('active');
-    btnEnviar.classList.add('active');
+    if(cajaBuscador.style.visibility === 'visible'){ esconderBuscador() } else { mostrarBuscador() }
 })
 
 btnEnviar.addEventListener('click', () => {
-    boxBuscador.classList.remove('active');
-    btnBuscar.classList.remove('active');
-    btnEnviar.classList.remove('active');
-    input.value = '';
+    if(navegador.classList.contains('activo')){
+        console.log('Buscando...')
+    } else {
+        esconderBuscador();
+    }
 })
 
-const resize = () => {
-    if(innerWidth < 1000){
-        btnMenu.style.left = '20px';
-        btnMenu.style.opacity = 1;
+//Navegador
+function mediaQueries(x) {
+    if (x.matches) {
+        nombre.style.zIndex = '0';
+        navegador.classList.add('activo');
+        navegador.classList.add('hide');
+        cajaBuscador.classList.add('activo');
+        btnBuscar.style.display = 'none';
+        btnMenu.style.display = 'inherit';
     } else {
-        btnMenu.style.left = 0;
-        btnMenu.style.opacity = 0;
+        nombre.style.zIndex = '3';
+        navegador.classList.remove('activo');
+        navegador.classList.remove('hide');
+        cajaBuscador.classList.remove('activo');
+        btnBuscar.style.display = 'inherit';
+        btnMenu.style.display = 'none';
     }
 }
-
-if(innerWidth < 1000) {
-    btnMenu.style.left = '20px';
-    btnMenu.style.opacity = 1;
-}
-addEventListener('resize', resize);
+btnMenu.addEventListener('click', () => {
+    navegador.classList.remove('hide');
+    navegador.style.setProperty("-webkit-filter", "drop-shadow(0 50px 200px #222)");
+})
+btnCerrar.addEventListener('click', () => {
+    navegador.classList.add('hide');
+    navegador.style.setProperty("-webkit-filter", "drop-shadow(0 0 0 transparent)");
+})
+  
+let x = window.matchMedia("(max-width: 1000px)")
+  mediaQueries(x);
+x.addListener(mediaQueries);
+//Scroll
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 200) {
+        encabezado.style.backgroundColor = 'white';
+    } else {
+        encabezado.style.backgroundColor = 'transparent';
+    }
+})
---
+++ js/slider.js
@@ -0,0 +1,3 @@
+const wrapper = document.querySelector('.wrapper-slider');
+const left = document.querySelector('.ico-left');
+const right = document.querySelector('.ico-right');
